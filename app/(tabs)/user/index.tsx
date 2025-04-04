7663e4e99447626f46480688154a583c990a3793
Huỳnh Vũ Khang <146689046+khang270607@users.noreply.github.com>
2025-04-05 06:58:04 +0700

    commit-1

--- app/(tabs)/user/index.tsx
+++ app/(tabs)/user/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Link } from 'expo-router';
 
 export default function TabTwoScreen() {
-  return <Text style={styles.title}>TRANG TÀI KHOẢN</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Trang người dùng</Text>
+    </View>
+  )
+
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
   },
-})
+  button: {
+    backgroundColor: '#28a745',
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
